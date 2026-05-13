5a4fb0ac5d922a62cea4e18e4113272d94c0a6bf
USER <USER@DESKTOP-0P43UTC>
2026-05-13 11:23:07 +0530

    JOHN

---
+++ src/test/java/RegisterModule/Register.java
@@ -0,0 +1,13 @@
+package RegisterModule;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Register {
+	@Test
+	public void register() {
+		Reporter.log("register to app");
+	}
+	
+
+}
