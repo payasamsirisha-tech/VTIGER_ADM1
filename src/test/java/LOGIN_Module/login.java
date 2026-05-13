1842bfa17f3ae15b9fc356627d3517e3ffe51dd8
USER <USER@DESKTOP-0P43UTC>
2026-05-13 11:19:44 +0530

    SAM

---
+++ src/test/java/LOGIN_Module/login.java
@@ -0,0 +1,13 @@
+package LOGIN_Module;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class login {
+	
+	@Test
+	public void login() {
+		Reporter.log("login success");
+	}
+
+}
